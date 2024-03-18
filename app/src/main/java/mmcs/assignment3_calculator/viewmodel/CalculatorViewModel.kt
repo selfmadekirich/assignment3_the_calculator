580af70ff93ae3e5667d6e59341c5316336a26ce
selfmadekirich <tosha.kirichenko.2001@mail.ru>
2024-03-19 00:36:25 +0300

    done

--- app/src/main/java/mmcs/assignment3_calculator/viewmodel/CalculatorViewModel.kt
+++ app/src/main/java/mmcs/assignment3_calculator/viewmodel/CalculatorViewModel.kt
@@ -2,31 +2,100 @@ package mmcs.assignment3_calculator.viewmodel
 
 import androidx.databinding.BaseObservable
 import androidx.databinding.ObservableField
+import kotlin.math.abs
+import kotlin.math.floor
 
 class CalculatorViewModel: BaseObservable(), Calculator {
     override var display = ObservableField<String>()
+    init{
+        display.set("0")
+    }
+
+    private var current = "0"
+    private var lastOp = ""
+    private var precalc = ""
+    private var last_was_sign = false
 
     override fun addDigit(dig: Int) {
-        TODO("Not yet implemented")
+        if(current == "0"){
+            current = ""
+        }
+        last_was_sign = false
+        current += dig.toString();
+        display.set(precalc + current)
     }
 
     override fun addPoint() {
-        TODO("Not yet implemented")
+        if(!current.contains('.'))
+            current += '.'
+        display.set(precalc + current)
+    }
+
+    private fun getOpStr(op: Operation):String{
+        when(op){
+            Operation.ADD -> return "+"
+            Operation.DIV -> return "/"
+            Operation.MUL -> return "*"
+            Operation.SUB -> return "-"
+        }
+        return ""
     }
 
     override fun addOperation(op: Operation) {
-        TODO("Not yet implemented")
+        if(lastOp != "" || last_was_sign)
+            return
+
+        if(current == "0"){
+               current = "-"
+               display.set(precalc + current)
+               return
+           }
+        last_was_sign = true
+
+        lastOp = getOpStr(op)
+        precalc += current + lastOp
+        current = ""
+        display.set(precalc)
+
     }
 
+    private fun calc():Double{
+        var prc =  precalc.toDouble();
+        var cur = current.toDouble();
+
+        when(lastOp){
+            "+" -> return prc + cur
+            "/" -> if(cur == 0.0) return 0.0 else return prc / cur
+            "*" -> return prc * cur
+            "-" -> return prc - cur
+        }
+        return 0.0
+    }
     override fun compute() {
-        TODO("Not yet implemented")
+        if(lastOp == "" || current == "0" || current == "")
+            return
+        current = precalc + current
+        var temp = current.split(lastOp)
+        precalc = temp[0]
+        current = temp[1]
+
+        var temp_f  = calc()
+        precalc = if(abs(temp_f - floor(temp_f)) > 0)  temp_f.toString() else temp_f.toInt().toString()
+        current = ""
+        lastOp = ""
+        last_was_sign = false
+        display.set(precalc)
     }
 
     override fun clear() {
-        TODO("Not yet implemented")
+        current = "0"
+        display.set(current)
     }
 
     override fun reset() {
-        TODO("Not yet implemented")
+        current = "0"
+        lastOp = ""
+        precalc = ""
+        display.set(current)
     }
 }
